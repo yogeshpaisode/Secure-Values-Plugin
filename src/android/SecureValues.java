46aad5a3bb4c19df198558aad3eafcadf3f7ce51
Yogesh D.Paisode <yogesh.paisode@gmail.com>
2016-05-25 10:46:39 +0530

    Update SecureValues.java

--- src/android/SecureValues.java
+++ src/android/SecureValues.java
@@ -1,13 +1,16 @@
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaArgs;
+import java.util.ArrayList;
+import java.util.List;
 public class SecureValues extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws org.json.JSONException {
-       
-        callbackContext.success("Hello Dude Yogesh Bro...");
-        
+        List list=new ArrayList();
+        list.add("http://192.168.0.43:111/gsfapi/");
+        JSONArray jSONArray=new JSONArray(list);
+        callbackContext.success(jSONArray);
         return true; //To change body of generated methods, choose Tools | Templates.
     }
     
