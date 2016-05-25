c288379991926368477e13838312bbd01f7d5b7e
Yogesh Paisode <yogesh@giis.com>
2016-05-25 09:58:40 +0530

    First commit

---
+++ src/android/SecureValues.java
@@ -0,0 +1,14 @@
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaArgs;
+public class SecureValues extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws org.json.JSONException {
+       
+        callbackContext.success("Hello Dude...");
+        
+        return true; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
